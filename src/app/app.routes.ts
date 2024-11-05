5a0bd65f2de055b8d49dee2fae8cfdb28a2aa1d4
shashanksola <solashashank1010@gmail.com>
2024-11-05 14:56:23 +0530

    routing works

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PlaceOrderComponent } from './components/place-order/place-order.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '', component: HomeComponent
+},
+{
+    path: 'order', component: PlaceOrderComponent
+}];
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Card } from '../../Card';
 
+
 @Component({
   selector: 'app-card',
   standalone: true,
@@ -8,6 +9,7 @@ import { Card } from '../../Card';
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
+
 export class CardComponent {
   @Input() item!: Card
   @Input() category!: String
